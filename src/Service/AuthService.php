5a33a1a72a70ac3760ce6acdb960fc21c457b2e7
ksenia <ksekat01@mail.ru>
2025-09-01 16:53:04 +0300

    Add logging of all key actions

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -7,6 +7,7 @@ use App\Entity\Session;
 use App\Entity\Token;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use App\Exception\ApiException;
@@ -14,19 +15,27 @@ use App\Exception\ApiException;
 class AuthService
 {
     public function __construct(
-        private EntityManagerInterface $em,
-        private UserPasswordHasherInterface $passwordHasher
-    ) {}
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher,
+        LoggerInterface $userLogger
+    ) {
+        $this->em = $em;
+        $this->passwordHasher = $passwordHasher;
+        $this->logger = $userLogger;
+    }
+
 
     public function login(LoginRequest $dto): array
     {
         $user = $this->em->getRepository(User::class)->findOneBy(['email' => $dto->email]);
 
         if (!$user || !$this->passwordHasher->isPasswordValid($user, $dto->password)) {
+            $this->logger->warning('Неудачная попытка логина', ['email' => $dto->email]);
             throw new ApiException('Неверный логин или пароль', 401);
         }
 
         if (!$user->isActive()) {
+            $this->logger->warning('Попытка логина заблокированного пользователя', ['email' => $dto->email]);
             throw new ApiException('Пользователь заблокирован', 403);
         }
 
@@ -55,13 +64,24 @@ class AuthService
             $this->em->flush();
             $this->em->commit();
 
+            $this->logger->info('Пользователь вошел в систему', [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'token' => $tokenValue
+            ]);
+
             return [
                 'token' => $tokenValue,
                 'expires_at' => $expiresAt->format('c'),
             ];
         } catch (\Throwable $e) {
             $this->em->rollback();
+            $this->logger->error('Ошибка при создании сессии/токена', [
+                'email' => $dto->email,
+                'error' => $e->getMessage()
+            ]);
             throw $e;
         }
     }
+
 }
--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -6,36 +6,49 @@ use App\Dto\User\RegisterUserRequest;
 use App\Entity\User;
 use App\Exception\ApiException;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserService
 {
     public function __construct(
-        private EntityManagerInterface $em,
-        private UserPasswordHasherInterface $passwordHasher
-    ) {}
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher,
+        LoggerInterface $userLogger
+    ) {
+        $this->em = $em;
+        $this->passwordHasher = $passwordHasher;
+        $this->logger = $userLogger;
+    }
 
     public function register(RegisterUserRequest $dto): User
     {
         $existingUser = $this->em->getRepository(User::class)->findOneBy(['email' => $dto->email]);
         if ($existingUser) {
+            $this->logger->warning('Попытка зарегистрировать уже существующий email', ['email' => $dto->email]);
             throw new ApiException('Пользователь с таким email уже существует', 409);
         }
+
         $this->em->wrapInTransaction(function () use ($dto) {
             $user = new User();
             $user->setEmail($dto->email);
-            $user->setPassword(
-                $this->passwordHasher->hashPassword($user, $dto->password)
-            );
+            $user->setPassword($this->passwordHasher->hashPassword($user, $dto->password));
             $user->setRoles([$dto->role]);
 
             $this->em->persist($user);
             $this->em->flush();
+
+            $this->logger->info('Новый пользователь зарегистрирован', [
+                'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'role' => $dto->role
+            ]);
         });
 
         return $this->em->getRepository(User::class)->findOneBy(['email' => $dto->email]);
     }
+
 
     public function getAllUsers(): array
     {
@@ -63,17 +76,29 @@ class UserService
 
     public function updateUser(User $user, array $data): User
     {
-        if (isset($data['email'])) {
+        $changes = [];
+        if (isset($data['email']) && $data['email'] !== $user->getEmail()) {
+            $changes['email'] = ['old' => $user->getEmail(), 'new' => $data['email']];
             $user->setEmail($data['email']);
         }
-        if (isset($data['roles'])) {
+        if (isset($data['roles']) && $data['roles'] !== $user->getRoles()) {
+            $changes['roles'] = ['old' => $user->getRoles(), 'new' => $data['roles']];
             $user->setRoles($data['roles']);
         }
-        if (isset($data['isActive'])) {
+        if (isset($data['isActive']) && $data['isActive'] !== $user->isActive()) {
+            $changes['isActive'] = ['old' => $user->isActive(), 'new' => (bool)$data['isActive']];
             $user->setIsActive((bool)$data['isActive']);
         }
 
         $this->em->flush();
+
+        if (!empty($changes)) {
+            $this->logger->info('Данные пользователя обновлены', [
+                'id' => $user->getId(),
+                'changes' => $changes
+            ]);
+        }
+
         return $user;
     }
 
@@ -81,6 +106,12 @@ class UserService
     {
         $user->setIsActive(false);
         $this->em->flush();
+
+        $this->logger->warning('Пользователь заблокирован', [
+            'id' => $user->getId(),
+            'email' => $user->getEmail()
+        ]);
+
         return $user;
     }
 
