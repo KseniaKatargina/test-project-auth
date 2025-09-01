31b23aff012c0006535a94621c10d60e3ecfe4d0
ksenia <ksekat01@mail.ru>
2025-09-01 13:52:46 +0300

    add ExceptionListener

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        $statusCode = $exception instanceof HttpExceptionInterface
+            ? $exception->getStatusCode()
+            : JsonResponse::HTTP_INTERNAL_SERVER_ERROR;
+
+        $response = new JsonResponse([
+            'status' => 'error',
+            'message' => $exception->getMessage(),
+        ], $statusCode);
+
+        $event->setResponse($response);
+    }
+}
